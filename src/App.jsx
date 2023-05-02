5d0e52d25fcdbe6d0a4c693dd4dde194a2886977
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-02 11:20:44 +0300

    Created the basic lay-out

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+import { Route, Routes } from "react-router";
+import Header from "./Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <h1>IKO KAZI KENYA</h1>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Header;
