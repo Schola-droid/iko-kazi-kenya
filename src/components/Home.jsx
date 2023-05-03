35d91168b51c3c7b432a73fff79406f2bb1a4265
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-03 21:43:44 +0300

    Edited home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import SearchBar from "./SearchBar";
 
 const Home = () => {
-    return (
-        <div>
-            HOME
-        </div>
-    );
-}
+  return (
+    <div id="home">
+      <div id="home-details">
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
