32f9d50b30bc3d019eead66d416e421f03cd27f0
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-03 16:56:26 +0300

    Updated jobs details view enabling to show only jobs list on the left

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -4,18 +4,28 @@ import CategoryFilter from "./CategoryFilter";
 import JobDetails from "./JobDetails";
 import { InitialContext } from "./JobContext";
 import JobsRender from "./JobsRender";
+import JobsList from "./JobsList";
 import Footer from "./Footer";
 
 const Jobs = () => {
   const { state, dispatch } = useContext(InitialContext);
   //   console.log(state.jobselect);
-  const id = state.jobselect ? "mainComponents" : "mainComponentsB";
+  let id;
+  let jobsview;
+  if (state.jobselect) {
+    id = "mainComponents";
+    jobsview = <JobsList />;
+  } else {
+    id = "mainComponentsB";
+    jobsview = <JobsRender />;
+  }
+
   return (
     <div>
       <SearchBar />
       <div id={id}>
         <CategoryFilter />
-        <JobsRender />
+        {jobsview}
         <JobDetails />
       </div>
       <Footer />
---
+++ src/components/JobsList.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { InitialContext } from "./JobContext";
+
+const JobsRender = () => {
+  const { state, dispatch } = useContext(InitialContext);
+
+  const data = state.jobs;
+  function jobselect(job) {
+    // console.log(job);
+    dispatch({ type: "JOB-SELECT", payload: job });
+  }
+  return (
+    <>
+      <div>
+        JOBS LIST
+        <ul id="jobslist-container">
+          {data?.map((job) => {
+            return (
+              <li
+                key={job.job_id}
+                className="jobslist"
+                onClick={() => jobselect(job)}
+              >
+                <h3>{job.job_title}</h3>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default JobsRender;
